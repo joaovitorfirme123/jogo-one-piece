1ce9bd533e33e5cd47dcae3e47e14e363578f06b
João Vitor <joaovitorfirme560@gmail.com>
2022-11-28 20:43:17 -0300

    atualização do jogo

--- js/javascript.js
+++ js/javascript.js
@@ -1,13 +1,32 @@
 const luffy = document.querySelector('.luffy');
+const trap = document.querySelector('.trap');
 
 const jump = () => {
     luffy.classList.add('jump');
 
     setTimeout(() =>{
         luffy.classList.remove('jump');
-    }, 900);
+    }, 800);
 }
 
+const loop = setInterval(() => {
+    console.log('loop')
+    const trapposition = trap.offsetLeft;
+    const luffyposition = +window.getComputedStyle(luffy).bottom.replace('px', '');
+    console.log(luffyposition);
+    if (trapposition < 120 &&  luffyposition < 95) {
+        
+        trap.style.animation = 'none';
+        trap.style.left = `${trapposition}px`;
+        luffy.style.animation = 'none';
+        luffy.style.bottom = `${luffyposition}px`;
+
+        luffy.src = './imagens/game-over.gif';
+        luffy.style.width = '100px';
+        
+        clearInterval(loop);
+    }
+},10);
 
 
 
